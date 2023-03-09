28d537a9cc92c1c8b77ce2486d6618b0a6d0ca6b
Neiman22 <pushkarev_av@inbox.ru>
2023-03-09 15:38:37 +0300

    array-deep-count.js

---
+++ array-deep-count.js
@@ -0,0 +1,7 @@
+function deepCount (array) {
+    return array.reduce((sum, cur) => {
+        return sum + (Array.isArray(cur) ? 1 + deepCount(cur) : 1)
+    }, 0)
+};
+
+console.log (deepCount([1, 2, [3, 4, [5]]]))
