d543ec4c86e5bd4aa7218330e4c9da0cc2b398a6
Neiman22 <pushkarev_av@inbox.ru>
2023-05-26 10:41:13 +0300

    10-2

---
+++ 10 Ошибки/10-2.js
@@ -0,0 +1,92 @@
+"use strict";
+console.log(`Пользовательские ошибки`);
+
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ValidationError';
+  }
+}
+
+function test() {
+  throw new ValidationError('Упс');
+}
+
+function readUser(json) {
+  let user = JSON.parse(json);
+  if (!user.name) throw new ValidationError("Нет поля: name");
+  if (!user.age) throw new ValidationError("Нет поля: age");
+  return user;
+}
+
+try {
+  test();
+} catch (error) {
+  console.log(error.message);  //Упс!
+  console.log(error.name); //ValidationError
+  console.log(error.stack); //список вложенных вызовов с номерами строк для каждого
+  console.log('\n');
+}
+
+//Расширение
+try {
+  let user =  readUser('{ "age": 25 }');
+} catch (err) {
+  if (err instanceof ValidationError) {
+    console.log("Некорректные данные: " + err.message); // Некорректные данные: Нет поля: name
+  } else if (err instanceof SyntaxError) {
+    console.log("JSON Ошибка Синтаксиса: " + err.message);
+  } else {
+    throw err;
+  }
+  console.log('\n');
+}
+
+//Наследование
+class PropertyRequiredError extends ValidationError {
+  constructor(property) {
+    super("Нет свойства: " + property);
+    this.name = "PropertyRequiredError";
+    this.property = property;
+  }
+}
+
+function readUser2(json) {
+  let user = JSON.parse(json);
+  if (!user.name) throw new PropertyRequiredError("name");
+  if (!user.age) throw new PropertyRequiredError("age");
+  return user;
+}
+
+try {
+  let user = readUser2('{ "age": 25 }');
+} catch (err) {
+  if (err instanceof ValidationError) {
+    console.log("Неверные данные: " + err.message); //Неверные данные: Нет свойства: name
+    console.log(err.name); //PropertyRequiredError
+    console.log(err.property); //name
+  } else if (err instanceof SyntaxError) {
+    console.log("Ошибка синтаксиса JSON: " + err.message);
+  } else {
+    throw err;
+  }
+}
+console.log('\n');
+
+//Task1
+console.log('Task1');
+
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message)
+    this.name = 'FormatError'
+  }
+}
+let err = new FormatError("ошибка форматирования");
+
+console.log(err.message); //ошибка форматирования
+console.log(err.name); //FormatError
+console.log(err.stack); //stack
+
+console.log( err instanceof FormatError );
+console.log( err instanceof SyntaxError );
